490a6284868275b745a62601acaa5677116a38fc
Maxime Chatelle <xakz@rxsoft.eu>
2017-03-18 23:36:47 +0100

    Simplifies building

---
+++ build.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+BUILD_DIR="../master"
+
+# sync static files
+rsync -av --exclude '.git' --delete "static/" "$BUILD_DIR/"
+
+# build elm app
+elm-make elm/Main.elm --warn --output "$BUILD_DIR/main.js"
+
