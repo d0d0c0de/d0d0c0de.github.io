92144cbe2c2d3a38ddd4c0c0fe92f424a1b56732
Maxime Chatelle <xakz@rxsoft.eu>
2017-03-25 12:10:04 +0100

    Switch to SCSS instead of pure CSS

--- build.sh
+++ build.sh
@@ -8,3 +8,7 @@ rsync -av --exclude '.git' --delete "static/" "$BUILD_DIR/"
 # build elm app
 elm-make elm/Main.elm --warn --output "$BUILD_DIR/main.js"
 
+# build CSS
+echo "Building main.css from SCSS files..."
+sassc scss/main.scss "$BUILD_DIR/main.css"
+
